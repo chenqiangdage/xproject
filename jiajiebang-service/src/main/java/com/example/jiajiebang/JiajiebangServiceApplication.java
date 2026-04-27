bfc097aca541b2212c5582a2d7474d2eb5947126
cqling <chenqiangdage@foxmail.com>
2026-04-27 23:35:27 +0800

    家洁帮demo API

--- jiajiebang-service/src/main/java/com/example/jiajiebang/JiajiebangServiceApplication.java
+++ jiajiebang-service/src/main/java/com/example/jiajiebang/JiajiebangServiceApplication.java
@@ -1,11 +1,13 @@
 package com.example.jiajiebang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.example.jiajiebang.mapper")
 public class JiajiebangServiceApplication {
 
     public static void main(String[] args) {
